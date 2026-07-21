daa944bd306386a99daa4201bf58a7a558068ca5
Kartikbodke <kartikbodkekb@gmail.com>
2026-07-21 23:14:11 +0530

    Day 1 done , LLM error not solved yet

--- backend/app/services/ai_services.py
+++ backend/app/services/ai_services.py
@@ -1,10 +1,36 @@
+from google import genai
+
+from app.core.config import GEMINI_API_KEY, GEMINI_MODEL
+
+
 class AIService:
 
+    def __init__(self):
+        self.client = genai.Client(api_key=GEMINI_API_KEY)
+
     def generate_response(self, question:str, level:str):
+
+        prompt = f"""
+        You are RoboMentor AI.
+
+        The student level is:
+        {level}
+
+        Answer the following robotics question clearly.
+
+        Question:
+        {question}
+        """
+
+        response = self.client.models.generate_content(
+            model=GEMINI_MODEL,
+            contents=prompt,
+        )
 
         return {
             "question": question,
             "level": level,
-            "answer": f"This is a dummy reponse for '{question}'."
+            "answer": response.text
         }
+    
 ai_service = AIService()
