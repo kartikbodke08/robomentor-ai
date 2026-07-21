919ff571ecf31c9c01f9d7b4a9baa9a4b4887692
Kartikbodke <kartikbodkekb@gmail.com>
2026-07-21 22:45:31 +0530

    ARchitecture and api building started

---
+++ backend/app/api/chat.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, APIRouter
+
+from app.schemas.chat import ChatRequest
+from app.services.ai_services import ai_service
+
+router = APIRouter()
+
+@router.post("/chat")
+def chat(request: ChatRequest) :
+    
+    return ai_service.generate_response(
+        question = request.question,
+        level = request.level 
+    )
+
+
+
